ca3ff18915ab12536e32d85708661536f1bd69d9
Nadia <nadikorableva@gmail.com>
2022-08-10 20:20:04 -0400

    button

--- client/src/components/Collages/PaletteFromPicture.js
+++ client/src/components/Collages/PaletteFromPicture.js
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 const PaletteFromPicture = () => {
     const [images, setImages] = useState([]);
     const [imageURLs, setImageURLs] = useState([]);
-
+    const [palette, setPalette] =useState(null);
     useEffect(() => {
 
         if (images.length<1) return;
@@ -18,22 +18,29 @@ const PaletteFromPicture = () => {
     const onImageChange = (e) => {
         setImages([...e.target.files]);
     }
-
+console.log("imagesurl", imageURLs)
     return (
         <Wrapper>
-            <input type="file" multiple accept="image/*" onChange={onImageChange}/>
-            {imageURLs?.map(imageSrc => <ImgWrap><Img src={imageSrc}/></ImgWrap>)}
+        <input type="file" multiple accept="image/*" onChange={onImageChange}/>
+        <Btn>Generate Palette</Btn>
+        <CollageWrap>
+        {imageURLs?.map(imageSrc => <ImgWrap><Img src={imageSrc}/></ImgWrap>)}
+        <PaletteWrap>
 
+        </PaletteWrap>
+        </CollageWrap>
         </Wrapper>)
 }
-
+const CollageWrap = styled.div``;
+const PaletteWrap = styled.div``;
 const ImgWrap = styled.div`
 height: 300px;
 `;
 const Img = styled.img`
 height: inherit;
 `;
-
+const Btn = styled.button`
+`;
 const Wrapper = styled.div`
 `
 
